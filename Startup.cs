a54120116c49c5e8373de3e5edfe17a4e1e30a9a
Preston Lloyd <plloyd241@gmail.com>
2020-01-08 23:05:30 -0700

    Because this is going to be embedded, we're going to use SQLite

--- Startup.cs
+++ Startup.cs
@@ -29,11 +29,8 @@ namespace CouchSignal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            if (Env.IsDevelopment())
-            {
-                services.AddDbContext<DataContext>(opt => opt.UseSqlite(Config.GetConnectionString("DataContext")));
-            }
-
+            services.AddDbContext<DataContext>(opt => opt.UseSqlite(Config.GetConnectionString("DataContext")));
+            
             services.AddControllers();
         }
 

cf035b25a886fa06e6e55147569cee6938782d27
Preston Lloyd <plloyd241@gmail.com>
2020-01-08 22:56:02 -0700

    Seed db with default roles

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,6 +8,15 @@ namespace CouchSignal.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {}
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed Roles
+            modelBuilder.Entity<Role>().HasData(
+                new {ID = (long) 1, Name = "Admin"},
+                new {ID = (long) 2, Name = "User"},
+                new {ID = (long) 3, Name = "Device"});
+        }
+
         public DbSet<Device> Devices { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Task> Tasks { get; set; }
--- Migrations/20200109055522_SeedDatabase.Designer.cs
+++ Migrations/20200109055522_SeedDatabase.Designer.cs
@@ -2,14 +2,16 @@
 using CouchSignal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CouchSignal.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200109055522_SeedDatabase")]
+    partial class SeedDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

355189377f5ef8dd79932b31fa455db2d65ece56
Preston Lloyd <plloyd241@gmail.com>
2020-02-21 23:06:50 -0700

    Config db context

--- src/CouchSignal.API/Startup.cs
+++ src/CouchSignal.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using CouchSignal.Infrastructure.Data;
 
 namespace CouchSignal.API
 {
@@ -26,6 +27,9 @@ namespace CouchSignal.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<DeviceContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DeviceContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
